2b63f0350cf169d65a01666ac82e8cc9fd7a0ba9
Sascha Nowak <sascha.nowak@netlogix.de>
2014-05-05 15:22:16 +0200

    [TASK] Add autosuggest controller

---
+++ Resources/Public/Scripts/AutoSuggest.js
@@ -0,0 +1,38 @@
+/*global angular:false */
+(function (window, angular, undefined) {
+	'use strict';
+
+	/**
+	 * @name nx.solrajax
+	 */
+	var module = angular.module('nx.solrajax.autosuggest', [
+		'ui.bootstrap.typeahead',
+		'nx.angular.variables'
+	]);
+
+
+	module.value('nx.solrajax.autosuggest.targetPage', '/');
+	module.value('nx.solrajax.autosuggest.suggestUrl', '/');
+	module.controller('AutoSuggestCtrl', ['$scope', '$http', 'nx.solrajax.autosuggest.targetPage', 'nx.solrajax.autosuggest.suggestUrl', function($scope, $http, targetPage, suggestUrl) {
+
+		$scope.q = '';
+		$scope.suggestUrl = suggestUrl;
+
+		$scope.getSuggestions = function (search) {
+			return $http.get($scope.suggestUrl, {
+				params: {
+					q: search.toLowerCase()
+				}
+			}).then(function (ressult) {
+				return ressult.data.results;
+			});
+		};
+
+		$scope.submit = function () {
+			var queryString = this.q.name || this.q;
+			window.location.href = targetPage + '?q=' + queryString;
+		};
+
+	}]);
+
+}(window, angular));
