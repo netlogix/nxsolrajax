c26f5822dd7d2e67a7dc1ac4584bf11d403431cd
Sascha Nowak <sascha.nowak@netlogix.de>
2014-12-02 14:34:13 +0100

    [TASK] Add immediate record indexer

--- ext_tables.php
+++ ext_tables.php
@@ -6,4 +6,8 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Netlogix Solr Ajax Search');
 
+if (TYPO3_MODE == 'BE') {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'Netlogix\\Nxsolrajax\\Indexer\\ImmediateRecordIndexer';
+}
+
 ?>
