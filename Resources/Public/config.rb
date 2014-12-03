5fed02f6d8fdb83a16f141feac3201a0047ffc84
Sascha Nowak <sascha.nowak@netlogix.de>
2014-12-03 19:29:54 +0100

    [TASK] Add compass config.rb

---
+++ Resources/Public/config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+gem 'bootstrap-sass', '=3.3.0.1'
+require 'bootstrap-sass'
+
+# Sass Paths
+http_path = "/typo3conf/ext/nxsolrajax/Resources/Public/"
+
+# Sass Directories
+css_dir = "Styles"
+sass_dir = "../Private/Scss"
+images_dir = "Assets/Images"
+javascripts_dir = "Script"
+fonts_dir = "Assets/Fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = false
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+sourcemap = true
