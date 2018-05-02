cb6cce0392ba4e49d447b7335980e9ca594a9f87
Stephan Schuler <stephan.schuler@netlogix.de>
2018-05-02 15:48:31 +0200

    !!!TASK: Clear repo for extension reboot

--- Resources/Public/Scripts/Tests/Unit/SimpleDatepicker/SimpleDatepicker.spec.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	'use strict';
-
-	describe('SimpleDatepicker', function() {
-		beforeEach(module('netlogix.solrajax.simpleDatepicker'));
-	});
-
-})();
--- Resources/Public/Scripts/Tests/Unit/Solrajax/Solrajax.spec.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	'use strict';
-
-	describe('Solrajax', function() {
-		beforeEach(module('netlogix.solrajax.solrajax'));
-	});
-
-})();
