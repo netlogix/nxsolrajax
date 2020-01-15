e94c55cbde5cfc5479c55cbf66ed1aff264bac23
Sascha Nowak <sascha.nowak@netlogix.de>
2020-01-15 13:02:26 +0100

    FEATURE: Raise version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Netlogix Solr Ajax',
+    'title' => 'netlogix Solr Ajax',
     'description' => '',
     'category' => 'libs',
     'author' => 'Sascha Nowak',
@@ -16,12 +16,12 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '8.0.0',
+    'version' => '9.0.0',
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.1.99',
-            'solr' => '8.1.*',
+            'typo3' => '9.5.0-10.99.99',
+            'solr' => '10.0.*',
         ],
         'conflicts' => [],
         'suggests' => [],
