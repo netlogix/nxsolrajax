676a50e0b74fc2fea763f05d05be7761581d3ff3
Sascha Nowak <sascha.nowak@netlogix.de>
2020-01-21 17:50:10 +0100

    FEATURE: Add label function for better extendability

--- Classes/Domain/Search/ResultSet/Grouping/GroupItem.php
+++ Classes/Domain/Search/ResultSet/Grouping/GroupItem.php
@@ -6,10 +6,15 @@ use JsonSerializable;
 
 class GroupItem extends \ApacheSolrForTypo3\Solr\Domain\Search\ResultSet\Grouping\GroupItem implements JsonSerializable
 {
+    protected function getGroupLabel()
+    {
+        return $this->getGroupValue();
+    }
+
     public function jsonSerialize()
     {
         return [
-            'label' => $this->getGroupValue(),
+            'label' => $this->getGroupLabel(),
             'name' => $this->getGroupValue(),
             'totalResults' => $this->getAllResultCount(),
             'start' => $this->getStart(),
