e62033df4f3f12f0b4c5cf000e598f4b8c1c5249
Gregor Sievert <gregor.sievert@netlogix.de>
2022-03-30 13:58:23 +0200

    TASK: Add reset url to single facet option

--- Classes/Domain/Search/ResultSet/Facets/OptionBased/Options/Option.php
+++ Classes/Domain/Search/ResultSet/Facets/OptionBased/Options/Option.php
@@ -46,6 +46,39 @@ class Option extends \ApacheSolrForTypo3\Solr\Domain\Search\ResultSet\Facets\Opt
     }
 
     /**
+     * @return string
+     */
+    public function getResetUrl()
+    {
+        $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+
+        $settings = $this->getFacet()->getConfiguration();
+        if (isset($settings['linkHelper']) && is_a($settings['linkHelper'], SelfLinkHelperInterface::class, true)) {
+            /** @var SelfLinkHelperInterface $linkHelper */
+            $linkHelper = $objectManager->get($settings['linkHelper']);
+            if ($linkHelper->canHandleSelfLink($this)) {
+                return $linkHelper->renderSelfLink($this);
+            }
+        }
+
+        $searchUriBuilder = $objectManager->get(SearchUriBuilder::class);
+        $previousRequest = $this->getFacet()->getResultSet()->getUsedSearchRequest();
+
+        if ($settings['keepAllOptionsOnSelection'] == 1) {
+            return $searchUriBuilder->getRemoveFacetValueUri(
+                $previousRequest,
+                $this->getFacet()->getName(),
+                $this->getUriValue()
+            );
+        } else {
+            return $searchUriBuilder->getRemoveFacetUri(
+                $previousRequest,
+                $this->getFacet()->getName()
+            );
+        }
+    }
+
+    /**
      * @return array
      */
     function jsonSerialize()
@@ -57,7 +90,8 @@ class Option extends \ApacheSolrForTypo3\Solr\Domain\Search\ResultSet\Facets\Opt
             'selected' => $this->getSelected(),
             'links' => [
                 'self' => $this->getUrl(),
-            ]
+                'reset' => $this->getResetUrl(),
+            ],
         ];
     }
 }
