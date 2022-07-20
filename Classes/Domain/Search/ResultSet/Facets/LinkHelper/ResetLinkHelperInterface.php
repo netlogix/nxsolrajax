c2176a665b6230d9efbd977d4b36ea4439e19fee
Alexander Jahn <alexander.jahn@netlogix.de>
2022-07-20 14:20:56 +0200

    FEATURE: replace linkHelper with events
    This adds events when generating facet URLs.
    A compatibility replacement is added to get the same behaviour as before.
    
    The old implementation will generate deprecation warnings.

--- Classes/Domain/Search/ResultSet/Facets/LinkHelper/ResetLinkHelperInterface.php
+++ Classes/Domain/Search/ResultSet/Facets/LinkHelper/ResetLinkHelperInterface.php
@@ -4,9 +4,22 @@ namespace Netlogix\Nxsolrajax\Domain\Search\ResultSet\Facets\LinkHelper;
 
 use ApacheSolrForTypo3\Solr\Domain\Search\ResultSet\Facets\AbstractFacet;
 
+/**
+ * @deprecated use PSR-14 events
+ */
 interface ResetLinkHelperInterface
 {
+    /**
+     * @deprecated replace this with GenerateFacetResetUrlEvent
+     * @param AbstractFacet $facet
+     * @return bool
+     */
     public function canHandleResetLink(AbstractFacet $facet): bool;
 
+    /**
+     * @deprecated replace this with GenerateFacetResetUrlEvent
+     * @param AbstractFacet $facet
+     * @return string
+     */
     public function renderResetLink(AbstractFacet $facet): string;
 }
