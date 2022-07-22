2caf90a155d71976ee0199345a211f7a7eb1c76c
Alexander Jahn <alexander.jahn@netlogix.de>
2022-07-22 15:07:47 +0200

    PATCH: add more tests

---
+++ Tests/Unit/Domain/Search/ResultSet/SearchResultSetTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netlogix\Nxsolrajax\Tests\Unit\Domain\Search\ResultSet;
+
+use ApacheSolrForTypo3\Solr\Domain\Search\SearchRequest;
+use Netlogix\Nxsolrajax\Domain\Search\ResultSet\SearchResultSet;
+use Nimut\TestingFramework\MockObject\AccessibleMockObjectInterface;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class SearchResultSetTest extends UnitTestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function itCanForceAddingFacetData() {
+        /** @var SearchResultSet|MockObject|AccessibleMockObjectInterface $subject */
+        $subject = $this->getAccessibleMock(SearchResultSet::class, ['getPage'], [], '', false);
+        $subject->method('getPage')->willReturn(0);
+
+        self::assertFalse($subject->_call('shouldAddFacetData'));
+
+        $subject->forceAddFacetData(true);
+
+        self::assertTrue($subject->_call('shouldAddFacetData'));
+    }
+    /**
+     * @test
+     * @return void
+     */
+    public function itWillAddFacetDataIfHasResults() {
+        /** @var SearchResultSet|MockObject|AccessibleMockObjectInterface $subject */
+        $subject = $this->getAccessibleMock(SearchResultSet::class, ['getPage'], [], '', false);
+
+        self::assertFalse($subject->_call('shouldAddFacetData'));
+
+        $subject->method('getPage')->willReturn(1);
+
+        self::assertTrue($subject->_call('shouldAddFacetData'));
+    }
+}
